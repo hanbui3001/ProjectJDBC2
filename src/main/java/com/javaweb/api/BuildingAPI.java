911ed1ceece9c31696ee3421984ccb778832d9ac
Hanbui3001 <bronzegoemo@gmail.com>
2025-03-17 23:43:56 +0700

    insert

--- src/main/java/com/javaweb/api/BuildingAPI.java
+++ src/main/java/com/javaweb/api/BuildingAPI.java
@@ -17,7 +17,7 @@ public class BuildingAPI {
 	@GetMapping(value = "api/building/")
     public List<BuildingDTO> getBuilding(@RequestParam(name = "name", required = false) String name,
     		@RequestParam(name = "districtId", required = false) Long district,
-    		@RequestParam(name = "typeCode", required = false) List<String> typeCode){
+    		@RequestParam(name = "typeCode", required = false) List<String> typecode){
     	List<BuildingDTO> result = buildingService.findAll(name, district);
     	return result;
     }
